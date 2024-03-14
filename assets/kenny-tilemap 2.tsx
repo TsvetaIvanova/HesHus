08fcdcec822f6cd86aab04c38633b197c5fe7da5
luke-hc-jackson <luke.hc.jackson@gmail.com>
2024-03-14 15:03:06 +0000

    fix tilemap file path

--- assets/kenny-tilemap 2.tsx
+++ assets/kenny-tilemap 2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="kenny-tilemap" tilewidth="16" tileheight="16" spacing="1" tilecount="486" columns="27">
- <image source="kenney_rpgUrbanKit/Tilemap/tilemap.png" trans="000000" width="458" height="305"/>
+ <image source="kenny-tilemap.png" trans="000000" width="458" height="305"/>
  <tile id="16">
   <properties>
    <property name="collision" value=""/>
