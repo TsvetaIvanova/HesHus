6e5c2e46086d2a9b8071df63a89ae466fbf706d2
April <apriljack333@gmail.com>
2024-03-19 11:19:56 +0000

    main menu done, tweaked buttons

--- core/src/com/heshus/game/screens/states/MainMenuScreen.java
+++ core/src/com/heshus/game/screens/states/MainMenuScreen.java
@@ -13,37 +13,54 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.heshus.game.engine.HesHusGame;
 import com.heshus.game.engine.Play;
 
-import java.util.Random;
 public class MainMenuScreen implements Screen {
 
     final HesHusGame game;
-
+    private ExtendViewport extendViewport;
     private OrthographicCamera camera;
     private OrthogonalTiledMapRenderer renderer;
     private TiledMap map;
-    Vector2 goTo;
     int mapPixelWidth;
     int mapPixelHeight;
-    float clock;
     BitmapFont font;
     TextButton settingsButton;
-
+    TextButton quitButton;
+    TextButton newButton;
+    private Stage stage;
+    private Table mainTable;
+    int xSpeed;
+    int ySpeed;
     public MainMenuScreen(final HesHusGame game) {
         this.game = game;
-
+        //Map for background initialisation
+        map = new TmxMapLoader().load("testmap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map, 1 / 1f);
+        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
+        mapPixelWidth = layer.getWidth() * layer.getTileWidth() ; //just calculate the width and height of tilemap
+        mapPixelHeight = layer.getHeight() * layer.getTileHeight();
+        //Camera initialisation
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 400, 240);
+        camera.position.x=0;
+        camera.position.y=0;
+        xSpeed=1;//we will move camera by these each frame
+        ySpeed=1;
+        extendViewport = new ExtendViewport(camera.viewportWidth, camera.viewportHeight, camera);
 
         //set up font
         font = new BitmapFont(Gdx.files.internal("Fonts/monogram/pixel.fnt"), false);
-        font.getData().setScale(1.5F);
+        font.getData().setScale(.5F);
         font.setColor(Color.BLACK);
 
         //BUTTONS
@@ -53,29 +70,51 @@ public class MainMenuScreen implements Screen {
         TextureRegionDrawable buttonTextureRegionDrawable =new TextureRegionDrawable(buttonTextureRegion);
         TextButton.TextButtonStyle textButtonStyle = new TextButton.TextButtonStyle(buttonTextureRegionDrawable, buttonTextureRegionDrawable, buttonTextureRegionDrawable, font );
 
+        Texture newButtonTexture = new Texture("UI/wide_button.png");
+        TextureRegion newButtonTextureRegion= new TextureRegion(newButtonTexture, newButtonTexture.getWidth(), newButtonTexture.getHeight());
+        TextureRegionDrawable newButtonTextureRegionDrawable =new TextureRegionDrawable(newButtonTextureRegion);
+        TextButton.TextButtonStyle newTextButtonStyle = new TextButton.TextButtonStyle(newButtonTextureRegionDrawable, newButtonTextureRegionDrawable, newButtonTextureRegionDrawable, font );
+
+
         //Settings button:
         settingsButton = new TextButton("SETTINGS", textButtonStyle); //Set the button up
-        settingsButton.padBottom(10);
+        settingsButton.padBottom(7);//center text in graphic
+        settingsButton.setTransform(true);
+        settingsButton.setScale(1F);
         settingsButton.addListener(new InputListener() {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                 Gdx.app.exit();
                 return false;
             }
         });
+        //Quit button:
+        quitButton = new TextButton("QUIT :(", textButtonStyle); //Set the button up
+        quitButton.padBottom(6);
+        quitButton.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                Gdx.app.exit();
+                return false;
+            }
+        });
+        //New Game button
+        newButton = new TextButton("NEW GAME!!", newTextButtonStyle); //Set the button up
+        newButton.padBottom(6);
+        newButton.addListener(new InputListener() {
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.setScreen(new Play(game));
+                dispose();
+                return false;
+            }
+        });
 
-        goTo = new Vector2(0,0);
-        map = new TmxMapLoader().load("testmap.tmx");
-        renderer = new OrthogonalTiledMapRenderer(map, 1 / 1f);
-        TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
-        mapPixelWidth = layer.getWidth() * layer.getTileWidth() ;
-        mapPixelHeight = layer.getHeight() * layer.getTileHeight();
-
-        camera = new OrthographicCamera();
-        camera.setToOrtho(false, 400, 240);
-        camera.position.x=0;
-        camera.position.y=0;
-
-        clock =4;
+        mainTable = new Table();
+        mainTable.add(newButton).colspan(2).padBottom(3);
+        mainTable.row();
+        mainTable.add(settingsButton).padRight(3);
+        mainTable.add(quitButton);
+        mainTable.setFillParent(true);
+        stage = new Stage(extendViewport);
+        stage.addActor(mainTable);
     }
 
     /**
@@ -95,79 +134,33 @@ public class MainMenuScreen implements Screen {
     @Override
     public void render(float delta) {
             ScreenUtils.clear(0, 0.2f, 0, 1);
+            Gdx.input.setInputProcessor(stage);
+
             //tilemap
             renderer.setView(camera);
             renderer.render();
-            //text
-            game.batch.setProjectionMatrix(camera.combined);
-            game.batch.begin();
-            game.font.draw(game.batch, "Welcome to HeslingtonHustle!!! ", camera.position.x, camera.position.y);
-            game.font.draw(game.batch, "Tap anywhere to begin!", 100, 100);
 
+            //text
+            game.batch.begin();
+            font.draw(game.batch, "Welcome to HeslingtonHustle!!! ", camera.position.x- camera.viewportWidth/2, camera.position.y+ camera.viewportHeight/2);
             //buttons
-            settingsButton.setPosition(camera.position.x+camera.viewportWidth/6,camera.position.y+ camera.viewportHeight/3);
-            settingsButton.draw(game.batch, 1);
+            mainTable.setPosition(camera.position.x- camera.viewportWidth/2,camera.position.y- camera.viewportHeight/2);
+            stage.draw();
             game.batch.end();
 
-            if (Gdx.input.isTouched()) {
-                game.setScreen(new Play(game));
-                dispose();
+            //moving camera (if the map has a yOffSet or xOffSet i think it will break)
+            camera.position.x+=xSpeed;
+            camera.position.y+=ySpeed;
+            if(camera.position.y+ySpeed + camera.viewportHeight/2>= mapPixelHeight ||camera.position.y+ySpeed- camera.viewportHeight/2<=0 ){
+                ySpeed*=-1;
             }
-            //Camera
-            //int cameraSmoothness = 400; //higher looks smoother! makes it take longer for camera to reach player pos
-            //camera.position.set((goTo.x+(camera.position.x *(cameraSmoothness-1)))/cameraSmoothness, ((goTo.y/ 2)+(camera.position.y *(cameraSmoothness-1)))/cameraSmoothness, 0);
-
-            lockCameraInTiledMapLayer(camera,(TiledMapTileLayer) map.getLayers().get(0)); //locks camera position so it cannot show out of bounds
-            camera.position.set(Math.round(camera.position.x) ,Math.round(camera.position.y),0);//This is needed to stop black lines between tiles. I think something to do with the tilemaprenderer and floats causes this
+            if(camera.position.x+xSpeed + camera.viewportWidth/2>= mapPixelWidth ||camera.position.x+xSpeed- camera.viewportWidth/2<=0 ){
+                xSpeed*=-1;
+            }
             camera.viewportWidth = Math.round(camera.viewportWidth);
             camera.viewportHeight = Math.round(camera.viewportHeight);
             camera.update();
-            System.out.println(goTo.y);
-
-            clock+=delta;
-            if (clock>4){
-                if (camera.position.y>200||camera.position.y==0){
-                    goTo.y = 0;
-                    goTo.x = 0;
-                }
-                else {
-                    goTo.y =0;
-                    goTo.x =mapPixelWidth;
-                }
-                clock=0;
-            }
-
     }
-    private void lockCameraInTiledMapLayer(OrthographicCamera cam, TiledMapTileLayer layer){
-        //get variables needed to find edges of map!
-        int mapPixelOffsetY =(int) layer.getOffsetY();
-        int mapPixelOffsetX =(int) layer.getOffsetX();
-        int mapPixelWidth = layer.getWidth() * layer.getTileWidth() + mapPixelOffsetX;
-        int mapPixelHeight = layer.getHeight() * layer.getTileHeight() + mapPixelOffsetY;
-        //if the camera viewport is large enough to see outside the map on both sides at once, there is no useful way to lock it. this shouldn't happen
-        if (cam.viewportWidth>mapPixelWidth || cam.viewportHeight>mapPixelHeight){
-            return;
-        }
-
-        //check if camera would show out of bounds, lock it in bounds if it would
-        if ((cam.position.x- (cam.viewportWidth/2)< mapPixelOffsetX))//is the camera too far left.
-        {
-            cam.position.x = mapPixelOffsetX + cam.viewportWidth/2;
-        }
-        else if ((cam.position.x+ (cam.viewportWidth/2)> mapPixelWidth))//is the camera too far right.
-        {
-            cam.position.x = mapPixelWidth - cam.viewportWidth/2;
-        }
-        if ((cam.position.y- (cam.viewportHeight/2)< mapPixelOffsetY))//is the camera too low.
-        {
-            cam.position.y = mapPixelOffsetY + cam.viewportHeight/2;
-        }
-        else if ((cam.position.y+ (cam.viewportHeight/2)> mapPixelHeight))//is the camera too high.
-        {
-            cam.position.y = mapPixelHeight - cam.viewportHeight/2;
-        }
-    }
-
 
     /**
      * @param width
@@ -176,7 +169,7 @@ public class MainMenuScreen implements Screen {
      */
     @Override
     public void resize(int width, int height) {
-
+        extendViewport.update(width,height);
     }
 
     /**
@@ -192,7 +185,6 @@ public class MainMenuScreen implements Screen {
      */
     @Override
     public void resume() {
-
     }
 
     /**
